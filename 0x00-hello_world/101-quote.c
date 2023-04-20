99c8d1ba55d3e977e0b3861f545ffdb07ba09b8e
vanessa <vanessa.azenwa@gmail.com>
2023-04-20 14:41:27 -0700

    include description for function main, correct indentaion

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,14 @@
 
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 1 (Error)
+ */
+
 int main(void)
 {
-    write(2, MESSAGE, sizeof(MESSAGE) - 1);
-    return (1);
+	write(2, MESSAGE, sizeof(MESSAGE) - 1);
+	return (1);
 }
