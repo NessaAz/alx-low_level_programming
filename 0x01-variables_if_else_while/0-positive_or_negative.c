a5ca71e8ea1c22a01b05d2d00427b0b8d784b7f9
vanessa <vanessa.azenwa@gmail.com>
2023-04-24 11:15:16 -0700

    code willprint whether a number is zero, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/** 
+ * main - assigns a random number to variable n each time it is executed
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	printf("The number %d ", n);
+	if(n>0)
+		printf("is positive\n");
+	else if (n==0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
 	return (0);
 }
