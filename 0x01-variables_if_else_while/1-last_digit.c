a763ce201290c8816a359b37c565e900537e7fec
vanessa <vanessa.azenwa@gmail.com>
2023-04-24 11:36:30 -0700

    program generates a random number and extracts the last digit from it

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - prints the last digit of a randomly generated number
+ * Return: 0 upon successful completion
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	int lastDigit = n % 10;
+
+	printf("%d is ", n);
+	if (lastDigit > 5)
+        {
+            printf("%d and is greater than 5\n", lastDigit);
+        }
+        else if (lastDigit == 0)
+        {
+            printf("%d and is 0\n", lastDigit);
+        }
+        else
+        {
+            printf("%d and is less than 6 and not 0\n", lastDigit);
+        }
 	return (0);
 }
