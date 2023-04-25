874e8e69c60a52b9ef5bf8d42b53fb6124148250
vanessa <vanessa.azenwa@gmail.com>
2023-04-25 16:42:57 -0700

    fixes syntax error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks if a character is lowercase if YES it returns 1
  * otherwise returns 0 just like islower() function in ctype.h
- * @c: Is the int value to be compared with the ASCII value
+ * @c: Is the character to check
  * Return: 0 if successful. _islower - Entry point
  */
 
