a1bd31e6f27cfc8800612b9d890c7b33fc1eb3b9
vanessa <vanessa.azenwa@gmail.com>
2023-04-25 19:32:14 -0700

    refactors program

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,26 +4,52 @@
 /**
  * print_times_table - This program defines a function
  * which prints out a multiplication table of
- * integers from 0 to n inclusive, where 
+ * integers from 0 to n inclusive, where
  * @n: is the input parameter to the function.
- * The function only prints the table if n is between 0 and 15 inclusive. 
- * If n is outside of this range, the function returns without printing anything
+ * The function only prints the table if n is between 0 and 15 inclusive.
+ * If n is outside of this range, the function returns without 
+ * printing anything
  */
 
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	{
+	int row;
+	int column;
+	int product;
+
+	if (n < 0 || n >= 15)
 		return;
-	}
-
-	for (int i = 0; i <= n; i++)
+	for (row = 0; row <= n; row++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (column = 0; column <= n; column++)
 		{
-			printf("%4d", i * j);
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
 		}
-
-		printf("\n");
+		_putchar('\n');
 	}
 }
