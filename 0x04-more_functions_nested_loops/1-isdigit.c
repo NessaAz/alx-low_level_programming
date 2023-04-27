48698b3f3a464716ed63e9696ea47f900ba88a5d
vanessa <vanessa.azenwa@gmail.com>
2023-04-27 16:42:49 -0700

    program finds digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function checks for digits
+ * @c: parameter
+ * Return: either 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
+
