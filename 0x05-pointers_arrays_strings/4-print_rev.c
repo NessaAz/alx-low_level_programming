cb35a39ac5af4ce1b24454083f01bb1d6546c054
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-03 02:50:55 +0300

    indent trailing statement

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,8 @@ void print_rev(char *s)
 	int i, len;
 
 	/* Find length of string */
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
+		;
 
 	/* Print characters in reverse order */
 	for (i = len - 1; i >= 0; i--)
