56e31d620e0c10c43d8f7ef3db9224a289860dfa
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-05 02:07:50 +0300

    function encodes a string into leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into 1337(leet).
+ * @str: the string to encode.
+ * Return: a pointer to the resulting string.
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char leet_map[256] = {0};
+
+	/* Build the leet map */
+	leet_map['a'] = leet_map['A'] = '4';
+	leet_map['e'] = leet_map['E'] = '3';
+	leet_map['o'] = leet_map['O'] = '0';
+	leet_map['t'] = leet_map['T'] = '7';
+	leet_map['l'] = leet_map['L'] = '1';
+
+	/* Encode the string */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (leet_map[str[i]] != 0)
+		{
+			/* Replace the character with its leet counterpart */
+			str[i] = leet_map[str[i]];
+		}
+	}
+
+	return (str);
+}
