4c2dd59789962214e8c417367387fa20c1ce68cd
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-05 04:33:24 +0300

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,7 @@ char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(int c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 
 #endif /*MAIN_H */
