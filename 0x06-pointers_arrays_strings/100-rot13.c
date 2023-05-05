ff370ca4a0a307131d2c0638aa607aa6285ab2b2
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-05 03:49:29 +0300

    refactor

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,29 +10,18 @@
 char *rot13(char *str)
 {
 	int i, j;
-	char rot13_map[256] = {0};
+	char *src = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *dest = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	/* Build the rot13 map */
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		rot13_map[i] = i + 13;
-		if (rot13_map[i] > 'z')
-			rot13_map[i] -= 26;
-	}
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		rot13_map[i] = i + 13;
-		if (rot13_map[i] > 'Z')
-			rot13_map[i] -= 26;
-	}
-
-	/* Encode the string */
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (rot13_map[str[i]] != 0)
+		for (j = 0; src[j] != '\0'; j++)
 		{
-			/* Replace the character with its rot13 counterpart */
-			str[i] = rot13_map[str[i]];
+			if (str[i] == src[j])
+			{
+				str[i] = dest[j];
+				break;
+			}
 		}
 	}
 
