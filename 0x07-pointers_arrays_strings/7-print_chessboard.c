6e7c2f591fd23706084ad75b40316ee3234dcead
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-09 01:45:05 +0300

    refactor

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,7 +14,8 @@ void print_chessboard(char (*a)[8])
 	for (row = 0; row < 8; row++)
 	{
 		for (column = 0; column < 8; column++)
-			_putchar(a[row][column]);
-			_putchar("\n");
+
+		_putchar(a[row][column]);
+		_putchar("\n");
 	}
 }
