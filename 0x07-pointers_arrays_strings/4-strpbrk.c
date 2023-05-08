f856048e99db68ca8ca580764859e2552072a409
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-09 01:07:06 +0300

    refactor

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,8 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: the string to search
  * @accept: the set of bytes to search for
- * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * Return: a pointer to the byte in s that matches one of the
+ * bytes in accept,
  * or NULL if no such byte is found
  */
 
