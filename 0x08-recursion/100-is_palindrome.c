38aeb077f23e2462ffc5cdd647721be51f211b57
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 04:50:09 +0300

    define _strlen_recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 
 /**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to get the length of.
+ * Return: The length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return (0);
+    return (1 + _strlen_recursion(s + 1));
+}
+
+/**
  * is_palindrome - Checks if a string,s is a palindrome.
  * @s: The string to check.
   * Return: 1 if s is a palindrome, 0 otherwise.
