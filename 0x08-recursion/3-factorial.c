6f58fad42c65e8cfb498c201d764097c1c748f1f
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 04:16:54 +0300

    correct syntax

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,6 +3,7 @@
 /**
  * factorial - returns the factorial of a given number n
  * @n: the number to be 'factorialed'
+ * Return: the dactorial of n
 */
 
 int factorial(int n)
@@ -10,13 +11,13 @@ int factorial(int n)
 	if (n < 0)
 	{
 		return (-1);
-	} 
+	}
 	else if (n == 0)
 	{
 		return (1);
-	} 
+	}
 	else
 	{
-		return (n * factorial(n-1));
+		return (n * factorial(n - 1));
 	}
 }
