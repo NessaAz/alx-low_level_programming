a3b60378f7dde1744ffe5af6638e8b3bc998775e
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 05:49:03 +0300

    refactor with a divisibilityCheck function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,23 @@
 #include <math.h>
 
 /**
+ * divisibilityCheck - helper function that checks if a number is divisible
+ * by any number less than its square root
+ * @n: the number to check
+ * @divisor: the current divisor being tested
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int divisibilityCheck(int n, int divisor)
+{
+	if (divisor >= sqrt(n))
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (divisibilityCheck(n, divisor + 1));
+}
+
+/**
  * is_prime_number - checks if a number is prime
  * @n: the number to check
  * Return: 1 if n is prime, 0 otherwise
@@ -9,14 +26,8 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n < 2)
 		return (0);
 
-	if (n % i == 0)
-		return (0);
-	i++;
-
-	return (1);
+	return (divisibilityCheck(n, 2));
 }
