19d4763c4da790d1d6e11c26ab69b35f3fc3eec5
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 05:23:39 +0300

    correct syntax

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -24,7 +24,8 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 
 		/* Otherwise, try to match the '*' with a substring of s1 */
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		if (*s1 != '\0' && (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
+
 			return (1);
 	}
 
