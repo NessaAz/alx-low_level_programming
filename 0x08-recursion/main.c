a28cdbd1b136ba9961c46ef30d84140f0819fe98
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 06:01:04 +0300

    update test cases

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -3,15 +3,17 @@
 
 int main(void)
 {
-	int n, result;
+	int n = 17;
+	printf("%d\n", is_prime_number(n));
 
-	n = 5;
-	result = is_prime_number(n);
-	printf("%d is prime: %d\n", n, result);
+	n = 27;
+	printf("%d\n", is_prime_number(n));
 
-	n = 15;
-	result = is_prime_number(n);
-	printf("%d is prime: %d\n", n, result);
+	n = 2;
+	printf("%d\n", is_prime_number(n));
+
+	n = 1;
+	printf("%d\n", is_prime_number(n));
 
 	return (0);
 }
