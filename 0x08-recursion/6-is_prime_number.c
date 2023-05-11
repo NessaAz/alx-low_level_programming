5ad3a5b45b34edfd007b19f73f9514be2587f025
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-11 05:41:02 +0300

    use while loop instead of for loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,10 +14,11 @@ int is_prime_number(int n)
 	if (n < 2)
 		return (0);
 
-	for (int i = 2; i <= sqrt(n); i++)
+	while (i * i <= n)
 	{
 		if (n % i == 0)
 			return (0);
+		i++;
 	}
 
 	return (1);
