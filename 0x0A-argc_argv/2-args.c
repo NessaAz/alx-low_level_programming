33dcd4ab2d1b0b8daf9b112f7ebbd37935aafed3
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-13 01:24:02 +0300

    refactor using for loop

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: number of command-line arguments
- * @argv: array of pointers to strings containing the arguments
- * return: Always 0 (Success)
+ * main - Prints all arguments received
+ * @argc: Number of arguments received
+ * @argv: Array of pointers to the arguments
+ * Return: Always 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
-	{
+	int i;
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	}
 
 	return (0);
 }
