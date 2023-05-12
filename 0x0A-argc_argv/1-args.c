e185c437000dac043bb3eb050c4122bac38d0bd3
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-13 01:53:59 +0300

    modify return descr

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - prints the number of arguments passed to it
  * @argc: number of command-line arguments
  * @argv: array of pointers to strings containing the arguments
- * return: Always 0 (Success)
+ * return: 0 on success, non-zero on failure
  */
 
 int main(int argc, char *argv[])
