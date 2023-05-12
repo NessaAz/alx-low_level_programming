dd1ab9a9a6c11338bb91383b3fa53f8cebb46b80
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-13 00:19:43 +0300

    add header comment

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -  function prints the name of the program
+ * @argc: size of argv
+ * @argv: arguments
+ * return: (0) if successful 
+ */
+
 int main(int argc, char *argv[]) 
 {
 	printf("%s\n", argv[0]);
