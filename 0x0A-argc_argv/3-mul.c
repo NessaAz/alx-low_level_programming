1b8fc1bffe7442b7a54217d0413a293640974989
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-13 01:30:40 +0300

    function multiplies num1 & num2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies num1 & num2
+ * @argc: number of command-line arguments
+ * @argv: array of pointers to strings containing the arguments
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, product;
+
+	/* ensure its only 2 numbers*/
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
