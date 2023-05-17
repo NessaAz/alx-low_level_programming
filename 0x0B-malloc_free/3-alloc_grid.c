6be85f4d08c1e8d6b4b774bce22a68e894abea2a
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-17 17:12:46 +0300

    apply betty style

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,32 +10,33 @@
 
 int **alloc_grid(int width, int height)
 {
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    int **grid = malloc(sizeof(int *) * height);
+	int **grid = malloc(sizeof(int *) * height);
 
-    if (grid == NULL)
-        return (NULL);
+	if (grid == NULL)
+		return (NULL);
 
-    for (int i = 0; i < height; i++)
-    {
-        grid[i] = malloc(sizeof(int) * width);
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
 
-        if (grid[i] == NULL)
-        {
-            /* free previously allocated memory */
-            for (int j = 0; j < i; j++)
-            {
-                free(grid[j]);
-            }
-            free(grid);
-            return (NULL);
-        }
-        /* initialize elements to 0 */
-        for (int j = 0; j < width; j++)
-        grid[i][j] = 0;
-    }
+		if (grid[i] == NULL)
+		{
+			/* free previously allocated memory */
+			for (int j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		
+		/* initialize elements to 0 */
+		for (int j = 0; j < width; j++)
+		grid[i][j] = 0;
+	}
 
-    return (grid);
+	return (grid);
 }
