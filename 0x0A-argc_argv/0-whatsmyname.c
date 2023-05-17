1e94bb7a66a4bc2f3f93bd5529183ce9e69969de
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-17 22:54:17 +0300

    r-R

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - prints the name of the program & new line
  * @argc: number of command-line arguments
  * @argv: array of pointers to strings containing the arguments
- * return: Always 0
+ * Return: Always 0
  */
 
 int main(int argc, char **argv[])
