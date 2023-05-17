3708e5d8540c7f674425ce2f33683133262fabaf
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-17 16:43:13 +0300

    declare vars at the beginning

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,19 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	size_t len1, len2, size;
+	char *concatenated;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
-	size_t size = len1 + len2 + 1;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	size = len1 + len2 + 1;
 
-	char *concatenated = malloc(sizeof(char) * size);
+	concatenated = malloc(sizeof(char) * size);
 
 	if (concatenated == NULL)
 		return (NULL);
