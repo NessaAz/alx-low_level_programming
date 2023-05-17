13f2af66fa7f3eb1123038c52cd221fcf112c664
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-17 19:37:06 +0300

    refactor

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,49 +14,35 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	len = strlen(str);
 	words = malloc((len + 1) * sizeof(char *));
-
 	if (words == NULL)
 		return (NULL);
-
 	word_count = 0;
 	i = 0;
 	while (str[i] != '\0')
 	{
 		while (str[i] == ' ')
-		{
 			i++;
-		}
 		if (str[i] == '\0')
 			break;
 		j = i;
 		while (str[j] != ' ' && str[j] != '\0')
-		{
 			j++;
-		}
-
 		words[word_count] = malloc((j - i + 1) * sizeof(char));
 		if (words[word_count] == NULL)
 		{
 			while (word_count > 0)
-			{
 				free(words[--word_count]);
-			}
 			free(words);
 			return (NULL);
 		}
-
 		k = 0;
 		while (i < j)
-		{
 			words[word_count][k++] = str[i++];
-		}
 		words[word_count][k] = '\0';
 		word_count++;
 	}
 	words[word_count] = NULL;
-
-	return words;
+	return (words);
 }
