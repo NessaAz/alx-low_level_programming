ed72679a5c5166522d478f6ef022e3d9a5f683ec
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-17 16:38:11 +0300

    update betty syntax

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,22 +11,22 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);
-    size_t size = len1 + len2 + 1;
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	size_t size = len1 + len2 + 1;
 
-    char *concatenated = malloc(sizeof(char) * size);
+	char *concatenated = malloc(sizeof(char) * size);
 
-    if (concatenated == NULL)
-        return (NULL);
+	if (concatenated == NULL)
+		return (NULL);
 
-    strcpy(concatenated, s1);
-    strcat(concatenated, s2);
+	strcpy(concatenated, s1);
+	strcat(concatenated, s2);
 
-    return (concatenated);
+	return (concatenated);
 }
