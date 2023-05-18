015b8541d053489efe4460dd9b76e6737177869a
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-19 01:37:58 +0300

    refactor

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,35 +7,68 @@
  * @s1: first string
  * @s2: second string
  * @n: max number of characters to concatenate from s2
- * Return: pointer to newly allocated concatenated string, Null if failed
+ * Return: pointer to newly allocated concatenated string, NULL if failed
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = (s1 != NULL) ? strlen(s1) : 0;
-	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
+	/* Ceck if either s1 or s2 is null */
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 
-	/* adjust n if it's greater than or equal to len of s2 */
+	/* Calculate the lengths of s2 and s1 */
+	unsigned int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+
+	/* if n>= length of s2, use the entire s2 */
 	if (n >= len2)
 	{
 		n = len2;
 	}
+	/* Allocate memory for the concatenated string */
+	char *result = malloc((len1 + n + 1) * sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
 
-	/* allocate memory for the concatenated string */
+	/* Copy s1 to the result string */
+	memcpy(result, s1, len1);
+
+	/* Copy n characters from s2 to the result string */
+	memcpy(result + len1, s2, n);
+
+	/* Add a null terminator to the end of the result string */
+	result[len1 + n] = '\0';
+
+	/* Return the pointer to the allocated memory */
+	return (result);
+}
+/*
+{
+	unsigned int len1 = (s1 != NULL) ? strlen(s1) : 0;
+	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
+
+	/* adjust n if it's greater than or equal to len of s2 
+	if (n >= len2)
+		n = len2;
+
+	/* allocate memory for the concatenated string 
 	char *result = malloc((len1 + n + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 
-	/* copy s1 to the result string */
+	/* copy s1 to the result string 
 	if (s1 != NULL)
 		memcpy(result, s1, len1);
 
-	/* copy n characters from s2 to the result string */
+	/* copy n characters from s2 to the result string 
 	if (s2 != NULL)
 		memcpy(result + len1, s2, n);
 
-	/* add null terminator */
+	/* add null terminator 
 	result[len1 + n] = '\0';
 
 	return (result);
 }
+*/
