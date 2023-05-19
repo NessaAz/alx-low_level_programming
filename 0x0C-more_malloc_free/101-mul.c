04a100e9ff9dcb0cf1a3939d43e4554bad99c51f
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-19 04:20:43 +0300

    refactor

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,92 +4,164 @@
 #include <ctype.h>
 
 /**
+ * _atoi_digit - converts char to integer
+ * @x: char to be converted
+*/
+
+int _atoi_digit(char c)
+{
+	unsigned int result;
+
+	if (c >= '0' && c <= '9')
+		result = c - '0';
+	else
+		result = 0;
+
+	return (result);
+}
+
+/**
  * _isNumber - Checks if a string is a number
  * @str: The string to be checked
  * Return: 1 if the string is not a number, 0 otherwise
  */
 
-int _is_number(const char *str) 
+int _is_number(const char *str)
 {
 	while (*str)
 	{
-		if (!isdigit(*str))
+		if (*str < '0' || *str > '9')
 			return (0);
 		str++;
 	}
+
 	return (1);
 }
 
 
 /**
- * multiply_numbers - function multiplies 2 positive numbers
- * @num1: first positive number
- * @num2: second positive number
- * Return: pointer to array, 0 if failed
+ * _calloc - allocates memory of an array using malloc
+ * @nmemb: number of elements
+ * @size: total bytes
+ * Return: pointer to allocated array if success, NULL if failed
 */
 
-int multiply_numbers(const char *num1, const char *num2)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    int result = 0;
-    int i, j;
+	char *tab;
+	unsigned int i;
 
-    for (i = 0; num1[i] != '\0'; i++) {
-        if (!isdigit(num1[i]))
-            return -1;
+	tab = malloc(size * nmemb);
 
-        for (j = 0; num2[j] != '\0'; j++) {
-            if (!isdigit(num2[j]))
-                return -1;
+	if (tab == NULL)
+		return (NULL);
 
-            result += (num1[i] - '0') * (num2[j] - '0');
-            result *= 10;
-        }
-    }
+	for (i = 0; i < nmemb; i++)
+		tab[i] = '0';
 
-    result /= 10;
-    return (result);
+	return (tab);
 }
 
-void print_number(int number)
+
+/**
+ * mul_array - multiplies 2 arrays
+ * @a1: 
+ * @len1:
+ * @a2:
+ * @a3:
+ * @lena: 
+ * Return: 
+*/
+
+void *mul_array(const char *a1, int len1, char a2, char *a3, int lena)
 {
-    if (number == 0) {
-        _putchar('0');
-        return;
-    }
+	int mul = 0, i, k;
 
-    int reversed_number = 0;
-    while (number != 0) {
-        reversed_number = reversed_number * 10 + number % 10;
-        number /= 10;
-    }
+	k = lena - 1;
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		mul += (_atoi_digit(a1[i]) * _atoi_digit(a2)) + (_atoi_digit(a3[k]));
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
 
-    while (reversed_number != 0) {
-        _putchar(reversed_number % 10 + '0');
-        reversed_number /= 10;
-    }
+	while (mul != 0)
+	{
+		mul += _atoi_digit(a3[k]);
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+
+	return (a3);
 }
 
-int main(int argc, char *argv[]) 
+
+/**
+ * print_array - prints array
+ * @a: 
+ * @nb: 
+ * Return: 
+*/
+
+void print_array(const char *a, int nb)
 {
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    
-    if (argc != 3 || !_is_number(argv[1]) || !_is_number(argv[2])) {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return (98);
-    }
+	int i = 0;
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+	while (a[i] == '0' && (i + 1) < nb)
+		i++;
 
-    int result = multiply_numbers(argv[1], argv[2]);
-    print_number(result);
-    _putchar('\n');
+	for (; i < nb; i++)
+		_putchar(a[i]);
 
-    return (0);
+	_putchar('\n');
+}
+
+
+/**
+ * main - entry point
+ * @argc:
+ * @argv:
+ * Return: 
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, c, len1, len2, lenres;
+	char *tabres;
+
+	if (argc != 3 || !_is_number(argv[1]) || !_is_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	len1 = 0;
+	while (argv[1][len1])
+		len1++;
+
+	len2 = 0;
+	while (argv[2][len2])
+		len2++;
+
+	lenres = len1 + len2;
+	tabres = _calloc(lenres, sizeof(char));
+
+	if (tabres == NULL)
+	{
+		free(tabres);
+		return (0);
+	}
+
+	for (i = len2 - 1, c = 0; i >= 0; i--)
+	{
+		tabres = mul_array(argv[1], len1, argv[2][i], tabres, lenres - c);
+		c++;
+	}
+
+	print_array(tabres, lenres);
+	free(tabres);
+
+	return (0);
 }
