0d9382473cd8e0ae23203840884b49b4781c689e
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-19 05:28:46 +0300

    clean

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -50,31 +50,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/* Return the pointer to the allocated memory */
 	return (result);
 }
-/*
-{
-	unsigned int len1 = (s1 != NULL) ? strlen(s1) : 0;
-	unsigned int len2 = (s2 != NULL) ? strlen(s2) : 0;
-
-	/* adjust n if it's greater than or equal to len of s2 
-	if (n >= len2)
-		n = len2;
-
-	/* allocate memory for the concatenated string 
-	char *result = malloc((len1 + n + 1) * sizeof(char));
-	if (result == NULL)
-		return (NULL);
-
-	/* copy s1 to the result string 
-	if (s1 != NULL)
-		memcpy(result, s1, len1);
-
-	/* copy n characters from s2 to the result string 
-	if (s2 != NULL)
-		memcpy(result + len1, s2, n);
-
-	/* add null terminator 
-	result[len1 + n] = '\0';
-
-	return (result);
-}
-*/
