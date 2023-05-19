b8afaaf406c565794377451190e354128c7dd084
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-19 04:22:11 +0300

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int multiply_numbers(const char* num1, const char* num2);
+int main(int argc, char *argv[]);
+int _atoi_digit(char c);
+int _is_number(const char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *mul_array(const char *a1, int len1, char a2, char *a3, int lena);
+void print_array(const char *a, int nb);
 
 #endif /*MAIN_H */
