341bc2842bc3a16a99022d1beb7994effc438449
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-19 05:19:02 +0300

    refactor using custom memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @inp: input
+ * @size: total bytes
+ * Return: string,s
+ */
+char *_memset(char *s, char inp, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = inp;
+	}
+
+	return (s);
+}
+
+
 /**
  * _calloc - allocates memory of an array using malloc
  * @nmemb: number of elements
- * @size: total bytes
+ * @sizea: total bytes
  * Return: pointer to allocated array if success, NULL if failed
 */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int nmemb, unsigned int sizea)
 {
 	void *ptr;
 
 	/* Check if nmemb or size is 0 */
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0 || sizea == 0)
 	{
 		return (NULL);
 	}
 
 	/* Allocate memory for the array */
-	ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * sizea);
 
 	/* Check if malloc failed */
 	if (ptr == NULL)
@@ -28,7 +49,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 
 	/* Set the memory to zero */
-	memset(ptr, 0, nmemb * size);
+	_memset(ptr, 0, nmemb * sizea);
 
 	/* Return the pointer to the allocated memory */
 	return (ptr);
