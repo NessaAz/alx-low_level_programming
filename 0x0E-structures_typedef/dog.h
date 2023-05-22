9d4522f1307969309b577f122789f42173eebfb1
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-23 00:12:18 +0300

    add include guard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#if !defined(DOG_H)
+#define DOG_H
+
+/**
+ * struct dog - represents info about a dog
+ * @name: pointer to a character string representing the dog's name
+ * @age: pointer to a floating-point number representing the dog's name
+ * @owner: pointer to a character string representing the dog's owner
+*/
+
 struct dog
 {
     char *name;
     float *age;
     char *owner;
 };
+
+
+#endif // DOG_H
+
