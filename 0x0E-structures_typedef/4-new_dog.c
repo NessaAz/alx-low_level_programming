e8f5b3e074eee777e2e7f375ea8787d4b40ed62b
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-23 01:29:43 +0300

    shorten

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,18 +17,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
 	name_copy = strdup(name);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
 	owner_copy = strdup(owner);
 	if (owner_copy == NULL)
 	{
@@ -36,10 +33,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-
 	new_dog->name = name_copy;
     new_dog->age = age;
     new_dog->owner = owner_copy;
-
 	return (new_dog);
 }
