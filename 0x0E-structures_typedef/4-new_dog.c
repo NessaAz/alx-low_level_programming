d9e7b75ebe8435396f0bddcb485d0e6cf5004fd0
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-23 01:23:02 +0300

    shorten code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,33 +12,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy, *owner_copy;
-	int name_len, owner_len;
+    dog_t *new_dog;
+    char *name_copy, *owner_copy;
 
-	/* Check if name or owner is NULL */
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	/* Calculate the lengths of name and owner */
-	name_len = strlen(name);
-	owner_len = strlen(owner);
-
-	/* Allocate memory for the new dog */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate memory for name copy */
-	name_copy = malloc((name_len + 1) * sizeof(char));
+	name_copy = strdup(name);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	/* Allocate memory for owner copy */
-	owner_copy = malloc((owner_len + 1) * sizeof(char));
+	owner_copy = strdup(owner);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
@@ -46,14 +37,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	/* Copy name and owner to the newly allocated memory */
-	strcpy(name_copy, name);
-	strcpy(owner_copy, owner);
-
-	/* Set the values of the new dog */
 	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner = owner_copy;
+    new_dog->age = age;
+    new_dog->owner = owner_copy;
 
 	return (new_dog);
 }
