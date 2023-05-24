0c2abc91c8668f72a23be02f621745618a8ed029
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-25 00:02:57 +0300

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: string
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+        f(name);
+}
