8dba1a6c239d675c7ab515d413166445ffdbf580
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-25 02:55:38 +0300

    undo

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,13 +5,10 @@
  * print_name - prints a name
  * @name: string
  * @f: function pointer
- * Return: none
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-    if (!name || !f)
-        return;
-
-    f(name);
+    if (name != NULL && f != NULL)
+        f(name);
 }
