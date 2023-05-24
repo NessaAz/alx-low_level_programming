30486c3f80a63cee191a993dc2ffac4f823c83cb
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-25 02:26:06 +0300

    fix syntax

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,33 +10,33 @@
 
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    char *ptr = (char *) main;
+	int num_bytes, i;
+	char *ptr = (char *) main;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    num_bytes = atoi(argv[1]);
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    printf("\n");
+	printf("\n");
 
-    /* Print the opcodes of the main function */
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x ", ptr[i] & 0xFF);
-        if (i != num_bytes - 1)
-            printf(' ');
-    }
+	/* Print the opcodes of the main function */
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", ptr[i] & 0xFF);
+		if (i != num_bytes - 1)
+			printf(' ');
+	}
     
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,29 +4,29 @@
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: The operator passed as an argument
- *
  * Return: A pointer to the function that corresponds to the operator,
- *         NULL if the operator is not found
+ * NULL if the operator is not found
  */
 int (*get_op_func(char *s))(int, int)
 {
-    int i = 0;
+	int i = 0;
 
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] =
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s && s[1] == '\0')
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
