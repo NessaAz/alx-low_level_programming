96cca3264e9c521d143c5581f4500e17efda9661
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-25 03:21:34 +0300

    refactor4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,31 +11,26 @@
 int main(int argc, char *argv[])
 {
 	int num_bytes, i;
-	char *ptr = (char *) main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 
 	num_bytes = atoi(argv[1]);
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
 
-	printf("\n");
-
+    unsigned char *main_ptr = (unsigned char *)main;
 	/* Print the opcodes of the main function */
 	for (i = 0; i < num_bytes; i++)
 	{
-		printf("%02x ", ptr[i] & 0xFF);
-		if (i != num_bytes - 1)
-			printf(' ');
+		printf("%02x ", main_ptr[i]);
 	}
-    
 	printf("\n");
 
 	return (0);
