31370ce5c11db93ed27bbef2d48e33ab09e26a34
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-26 13:12:23 +0300

    print values of different types:cahr,int,float,str

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints values of different types: cahr,int,float,str
+ * @format: A list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char curr_format;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        curr_format = format[i];
+
+        switch (curr_format)
+        {
+            case 'c':
+                printf("%c", va_arg(args, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(args, double));
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(nil)";
+                printf("%s", str);
+                break;
+            default:
+                i++;
+                continue;
+        }
+
+        if (format[i + 1] != '\0')
+            printf(", ");
+
+        i++;
+    }
+
+    va_end(args);
+
+    printf("\n");
+}
