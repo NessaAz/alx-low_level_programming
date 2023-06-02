e5a5fa9d44b1bf4390b61cea9409ebf17866f0d8
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 01:20:41 +0300

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,13 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t lenght = 0;
+	size_t lenght = 0;
 
-    while (h != NULL)
-    {
-        lenght++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		lenght++;
+		h = h->next;
+	}
 
-    return (lenght);
+	return (lenght);
 }
