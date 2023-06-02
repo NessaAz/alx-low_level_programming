fa88cd1064cd6715c1c34bd91d1ed8a175108444
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 02:06:15 +0300

    function to be executed before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * func_zero - function to be executed before main function
+*/
+
+void __attribute__ ((constructor)) func_zero()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
