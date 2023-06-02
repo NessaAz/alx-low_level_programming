0828fc0acb02a694c7d3eff38cd3d7801d453f63
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 02:08:54 +0300

    refactor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@
 
 void __attribute__ ((constructor)) func_zero()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+    printf("\nI bore my house upon my back!\n");
 }
