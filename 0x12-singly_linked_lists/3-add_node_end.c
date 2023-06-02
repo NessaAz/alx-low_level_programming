ebf564502a0d67db75fb1e9aa9bdbc72003a9078
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 01:52:45 +0300

    fn adds new node at the end of list_t

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add new node at the end of list_t
+ * @head: pointer to the head of list_t
+ * @str: string to be duplicated and stored in created_node
+ * Return: address of created_node, NULL if otherwise
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *created_node, *current;
+
+	created_node = malloc(sizeof(list_t));
+	if (created_node == NULL)
+		return (NULL);
+
+	created_node->str = strdup(str);
+	if (created_node->str == NULL)
+	{
+		free(created_node);
+		return (NULL);
+	}
+
+	created_node->len = strlen(str);
+	created_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = created_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = created_node;
+	}
+
+	return (created_node);
+}
