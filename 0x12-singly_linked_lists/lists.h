2fe6bf8a083d410375766b87dc73582a153deea8
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 01:08:01 +0300

    include length of node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,11 +7,13 @@
 /**
  * struct node - represents a node in a linked list
  * @str: string data stored in the node
+ * @len: length of the node
  * @next: pointer to next node of list_t
 */
 typedef struct node
 {
 	char *str;
+    int len;
 	struct node *next;
 } list_t;
 
