2c4fe0be101e13b37bd482b89fbfbaa0a7f36b77
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 02:00:30 +0300

    betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,14 @@
 
 void free_list(list_t *head)
 {
-    list_t *current_node, *next_node;
+	list_t *current_node, *next_node;
 
-    current_node = head;
-    while (current_node != NULL)
-    {
-        next_node = current_node->next;
-        free(current_node->str);
-        free(current_node);
-        current_node = next_node;
-    }
+	current_node = head;
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node->str);
+		free(current_node);
+		current_node = next_node;
+	}
 }
