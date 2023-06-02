28966e4eff4e4cd1886163183cc59baf38660aa3
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-03 00:49:22 +0300

    betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,14 +3,15 @@
 /**
  * print_list - prints the value of each node's str
  * @h: pointer to the head of list_h
- * Return - cnount ie, number of nodes traversed
+ * Return - count ie, number of nodes traversed
 */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-    
-	while (h != NULL) {
+
+	while (h != NULL)
+	{
 		if (h->str != NULL)
 		{
 			printf("%s\n", h->str);
@@ -23,6 +24,6 @@ size_t print_list(const list_t *h)
 		h = (*h).next;
 		count++;
 	}
-    
+
 	return (count);
 }
