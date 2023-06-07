6aafe9eb7266fef0c71c9ddbc1c9b349aee20433
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-07 20:05:30 +0300

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,36 +10,36 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *current_node;
-    unsigned int i;
+	listint_t *new_node, *current_node;
+	unsigned int i;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    if (idx == 0)
-    {
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-            return NULL;
-        new_node->n = n;
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    current_node = *head;
-    for (i = 0; i < idx - 1 && current_node != NULL; i++)
-        current_node = current_node->next;
+	current_node = *head;
+	for (i = 0; i < idx - 1 && current_node != NULL; i++)
+		current_node = current_node->next;
 
-    if (current_node == NULL)
-        return (NULL);
+	if (current_node == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = n;
-    new_node->next = current_node->next;
-    current_node->next = new_node;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = current_node->next;
+	current_node->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
