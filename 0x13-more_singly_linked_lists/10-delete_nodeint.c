3a24c50e294ce553f0651da686b4d9b45311291a
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-07 20:17:26 +0300

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,31 +9,30 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *current_node, *temporary_pointer;
-    unsigned int i;
+	listint_t *current_node, *temporary_pointer;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        temporary_pointer = *head;
-        *head = (*head)->next;
-        free(temporary_pointer);
-        return (1);
-    }
+	if (index == 0)
+	{
+		temporary_pointer = *head;
+		*head = (*head)->next;
+		free(temporary_pointer);
+		return (1);
+	}
 
-    current_node = *head;
-    for (i = 0; i < index - 1 && current_node
-     != NULL; i++)
-        current_node = current_node->next;
+	current_node = *head;
+	for (i = 0; i < index - 1 && current_node != NULL; i++)
+		current_node = current_node->next;
 
-    if (current_node == NULL || current_node->next == NULL)
-        return (-1);
+	if (current_node == NULL || current_node->next == NULL)
+		return (-1);
 
-    temporary_pointer = current_node->next;
-    current_node->next = current_node->next->next;
-    free(temporary_pointer);
+	temporary_pointer = current_node->next;
+	current_node->next = current_node->next->next;
+	free(temporary_pointer);
 
-    return (1);
+	return (1);
 }
