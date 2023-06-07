4c95c6feb40267071f6ba48bf6a7d2b1fc38cdf1
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-07 06:06:12 +0300

    prints linked list, even if it contains a loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints linked list, even if it contains a loop
+ * @head: pointer to the head of the list
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tort, *hare;
+
+	if (head == NULL)
+		return (0);
+
+	tort = hare = head;
+
+	while (hare && hare->next)
+	{
+		tort = tort->next;
+		hare = hare->next->next;
+
+		if (tort == hare)
+		{
+			printf("[%p] %d\n", (void *)tort, tort->n);
+			printf("[%p] %d\n", (void *)hare->next, hare->next->n);
+			exit(98);
+		}
+	}
+
+	size_t count = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		head = head->next;
+	}
+
+	return (count);
+}
