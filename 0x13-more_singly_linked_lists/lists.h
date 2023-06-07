f4667ec7b35b20df8a063c79f9cefce67b32e947
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-07 18:53:39 +0300

    update prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,6 +26,7 @@ size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
 
