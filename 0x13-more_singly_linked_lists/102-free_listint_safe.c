bf4a69b35f47ccf7afb78f91a3a68bd1c35d5723
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 00:27:01 +0300

    refactor

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,46 +2,28 @@
 
 /**
  * free_listint_safe - frees a listint_t list, even if it contains a loop
- * @h: pointer to a pointer to the head of the list.
- * Return: size of list that was freed.
+ * @h: pointer to the pointer to the head of the list
+ * Return: The number of nodes in the list
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tort, *hare, *temporary;
+	listint_t *current_node, *temporary_node;
 	size_t count = 0;
 
+
 	if (h == NULL || *h == NULL)
-		return (0);
+		return (count);
 
-	tort = *h;
-	hare = (*h)->next;
-
-	while (hare && hare->next && hare != tort)
+	current_node = *h;
+	while (current_node != NULL)
 	{
-		tort = tort->next;
-		hare = hare->next->next;
-	}
+		temporary_node = current_node;
+		current_node = current_node->next;
+		free(temporary_node);
 
-	if (hare == tort)
-	{
-		tort = *h;
-		while (tort != hare->next)
-		{
-			temporary = tort;
-			tort = tort->next;
-			free(temporary);
-			count++;
-		}
-		free(hare->next);
-		count++;
-	}
-
-	while (*h != NULL)
-	{
-		temporary = *h;
-		*h = (*h)->next;
-		free(temporary);
-		count++;
+		if (temporary_node <= current_node)
+			break;
 	}
 
 	*h = NULL;
