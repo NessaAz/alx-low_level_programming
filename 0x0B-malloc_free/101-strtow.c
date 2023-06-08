8c44d3021ef4c8fb3d7159d543eed7a406f99eea
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 02:04:48 +0300

    undo today's changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,67 +5,46 @@
 /**
  * strtow - splits a string into individual words
  * @str: the string to be split
- * Return: all words as string if success, NULL if failed
+ * Return: 0 if success, NULL if failed
 */
 
 char **strtow(char *str)
 {
-	int len;
-	int word_count;
-	int i, j;
-	int word_index, word_start, word_len;
-	char **all_words;
+	char **words;
+	int i, j, k, len, word_count;
 
-	len = strlen(str);
-	word_count = 0;
-	
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	/*Count the number of words in the string*/
-	for (i = 0; i < len; i++)
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			word_count++;
-	}
-	all_words = malloc((word_count + 1) * sizeof(char *));
-	if (all_words == NULL)
+	len = strlen(str);
+	words = malloc((len + 1) * sizeof(char *));
+	if (words == NULL)
 		return (NULL);
-
-	word_index = 0;
-    word_start = 0;
-    word_len = 0;
-
-	for (i = 0; i <= len; i++)
+	word_count = 0;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		while (str[i] == ' ')
+			i++;
+		if (str[i] == '\0')
+			break;
+		j = i;
+		while (str[j] != ' ' && str[j] != '\0')
+			j++;
+		words[word_count] = malloc((j - i + 1) * sizeof(char));
+		if (words[word_count] == NULL)
 		{
-			if (word_len > 0)
+			if (word_count == 0)
 			{
-				all_words[word_index] = malloc((word_len + 1) * sizeof(char));
-				if (all_words[word_index] == NULL)
-				{
-					for (j = 0; j < word_index; j++)
-						free(all_words[j]);
-					free(all_words);
-					return (NULL);
-				}
-
-				strncpy(all_words[word_index], str + word_start, word_len);
-				all_words[word_index][word_len] = '\0';
-				word_index++;
-				word_len = 0;
+				free(words);
+				return (NULL);
 			}
 		}
-		 else
-		{
-			if (word_len == 0)
-				word_start = i;
-			word_len++;
-		}
+		k = 0;
+		while (i < j)
+			words[word_count][k++] = str[i++];
+		words[word_count][k] = '\0';
+		word_count++;
 	}
-
-	all_words[word_index] = NULL;
-
-	return (all_words);
+	words[word_count] = NULL;
+	return (words);
 }
