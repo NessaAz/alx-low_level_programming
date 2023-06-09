38e890c4b509823f2b972ca9dafa98375f6b7732
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 05:07:03 +0300

    fix betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /*
  * print_binary - prints the binary representation of a number
- * n: The number to be printed in binary representation
+ * @n: The number to be printed in binary representation
  */
 
 void print_binary(unsigned long int n)
