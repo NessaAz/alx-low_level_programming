9daf1ab8d631d292df3084f7f246a16ac6932041
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 05:02:42 +0300

    betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,21 +7,21 @@
 
 void print_binary(unsigned long int n)
 {
-    unsigned long int bitwise_mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int first_zeros = 1;
-    
-    while (bitwise_mask > 0)
-    {
-        if ((n & bitwise_mask) != 0)
-        {
-            first_zeros = 0;
-            printf("1");
-        }
-        else if (!first_zeros)
-        {
-            printf("0");
-        }
-        
-        bitwise_mask >>= 1;
-    }
+	unsigned long int bitwise_mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int first_zeros = 1;
+ 
+	while (bitwise_mask > 0)
+	{
+		if ((n & bitwise_mask) != 0)
+		{
+			first_zeros = 0;
+			printf("1");
+		}
+		else if (!first_zeros)
+		{
+		printf("0");
+		}
+
+		bitwise_mask >>= 1;
+	}
 }
