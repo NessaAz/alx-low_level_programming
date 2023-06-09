e95f32e18a83a13050238fc4505296528cc2e8f2
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 04:56:47 +0300

    prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * print_binary: prints the binary representation of a number
+ * n: The number to be printed in binary representation
+ */
+
+void print_binary(unsigned long int n) {
+    unsigned long int bitwise_mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+    int first_zeros = 1;
+    
+    while (bitwise_mask > 0)
+    {
+        if ((n & bitwise_mask) != 0)
+        {
+            first_zeros = 0;
+            printf("1");
+        }
+        else if (!first_zeros)
+        {
+            printf("0");
+        }
+        
+        bitwise_mask >>= 1;
+    }
+}
