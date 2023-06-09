08476b9209bf0f90aa99ee9d131eb58503a09842
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 06:10:09 +0300

    betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness: checks the endianness of the system
+ * get_endianness - checks the endianness of the system
  * Return: 0 if big endian, 1 if little endian.
  */
 
