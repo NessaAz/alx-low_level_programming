6cc62d85d7f77533ddab8bfe935189444660d275
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-09 05:13:22 +0300

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    if (b == NULL)
-        return (0);/*null string returns 0*/
+	if (b == NULL)
+		return (0);/*null string returns 0*/
 
-    unsigned int uint = 0;
-    int i = 0;
+	unsigned int uint = 0;
+	int i = 0;
 
-    while (b[i] != '\0')/*iterate through each character in the string*/
-    {
-        if (b[i] == '0' || b[i] == '1')/*check if character is 0 or 1*/
-        {
-            /**
-             * convert the character to its corresponding integer value (0 or 1)
-             * update the result by shifting the bits to the left
-            */
-            uint = uint * 2 + (b[i] - '0');
-            i++;
-        }
-        else
-        {
-            /* invalid character returns 0*/
-            return (0);
-        }
-    }
-
-    return (uint);
+	while (b[i] != '\0')/*iterate through each character in the string*/
+	{
+		if (b[i] == '0' || b[i] == '1')/*check if character is 0 or 1*/
+		{
+			/**
+			 * convert the character to its corresponding integer value (0 or 1)
+			 * update the result by shifting the bits to the left
+			 */
+			uint = uint * 2 + (b[i] - '0');
+			i++;
+		}
+		else
+		{
+			/* invalid character returns 0*/
+			return (0);
+		}
+	}
+	return (uint);
 }
