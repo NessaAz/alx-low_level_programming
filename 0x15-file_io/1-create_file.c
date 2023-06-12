e2843d8d977b603f2265c20bd145a8b66f984327
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-13 02:48:15 +0300

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,31 +9,31 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    FILE *file;
-    int fputs_result;
+	FILE *file;
+	int fputs_result;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    /* Open the file for writing (truncating it if it exists) */
-    file = fopen(filename, "w");
-    if (file == NULL)
-        return (-1);
+	/* Open the file for writing (truncating it if it exists) */
+	file = fopen(filename, "w");
+	if (file == NULL)
+		return (-1);
 
-    /* Write the content to the file if text_content is not NULL */
-    if (text_content != NULL)
-    {
-        fputs_result = fputs(text_content, file);
-        if (fputs_result == EOF)
-        {
-            fclose(file);
-            return (9-1);
-        }
-    }
+	/* Write the content to the file if text_content is not NULL */
+	if (text_content != NULL)
+	{
+		fputs_result = fputs(text_content, file);
+		if (fputs_result == EOF)
+		{
+			fclose(file);
+			return (9-1);
+		}
+	}
 
-    /* Close the file */
-    if (fclose(file) == EOF)
-        return (-1);
+	/* Close the file */
+	if (fclose(file) == EOF)
+		return (-1);
 
-    return (1);
+	return (1);
 }
