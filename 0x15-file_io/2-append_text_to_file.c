8565fc9c776c8498f30aae07e1e6c74c3eb97913
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-13 02:57:39 +0300

    betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,23 +9,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    FILE *file;
-    int fputs_result;
+	FILE *file;
+	int fputs_result;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content == NULL)
-        return (-1);
+	if (text_content == NULL)
+		return (-1);
 
-    file = fopen(filename, "a");
-    if (file == NULL)
-        return (-1);
+	file = fopen(filename, "a");
+	if (file == NULL)
+		return (-1);
 
-    fputs_result = fputs(text_content, file);
+	fputs_result = fputs(text_content, file);
 
-    if (fclose(file) == EOF)
-        return (-1);
+	if (fclose(file) == EOF)
+		return (-1);
 
-    return ((fputs_result == EOF) ? -1 : 1);
+	return ((fputs_result == EOF) ? -1 : 1);
 }
