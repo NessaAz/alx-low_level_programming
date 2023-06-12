d32c49f634556c767a21ec84e69ea31c10e823a1
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-13 02:22:00 +0300

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,41 +9,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE *file;
-    char *buffer;
-    ssize_t read_bytes, written_bytes;
+	FILE *file;
+	char *buffer;
+	ssize_t read_bytes, written_bytes;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    file = fopen(filename, "r");
-    if (file == NULL)
-        return (0);
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
 
-    buffer = (char *)malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-    {
-        fclose(file);
-        return (0);
-    }
+	buffer = (char *)malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
 
-    read_bytes = fread(buffer, sizeof(char), letters, file);
-    if (read_bytes <= 0)
-    {
-        fclose(file);
-        free(buffer);
-        return (0);
-    }
+	read_bytes = fread(buffer, sizeof(char), letters, file);
+	if (read_bytes <= 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
 
-    written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-    if (written_bytes <= 0 || written_bytes != read_bytes)
-    {
-        fclose(file);
-        free(buffer);
-        return (0);
-    }
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (written_bytes <= 0 || written_bytes != read_bytes)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
 
-    fclose(file);
-    free(buffer);
-    return (read_bytes);
+	fclose(file);
+	free(buffer);
+	return (read_bytes);
 }
